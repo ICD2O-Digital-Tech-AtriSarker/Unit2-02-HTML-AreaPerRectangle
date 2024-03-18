87e9866b41677dea3e7dc2097513d0d2f28ee7e1
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2024-03-18 15:22:36 +0000

    Main Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+// Script for Area & Perimeter 
+
+// Elements
+const lengthInput = document.getElementById("LengthInput");
+const widthInput = document.getElementById("WidthInput");
+const calculateBtn = document.getElementById("CalculateBtn");
+const areaResult = document.getElementById("AreaResult");
+const perimeterResult = document.getElementById("PerimeterResult");
+
+// Code for calculating area and perimeter
+function Calculate() {
+  let length = Number(lengthInput.value);
+  let width = Number(widthInput.value);
+
+  let area = length * width;
+  let perimeter = 2 * ( length + width );
+
+  areaResult.innerHTML = "" + area.toFixed(2) + " m<sup>2</sup>"
+  perimeterResult.innerHTML = "" + perimeter.toFixed(2) + " m"
+  return
+}
